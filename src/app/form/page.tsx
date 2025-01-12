7f8a27ad0e9bd14d6ecf83036629ddfcfa650399
SarthakDaga21 <sarthakdaga.official@gmail.com>
2025-01-12 11:22:53 +0530

    Process env error solve

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function form() {
+
+  const [Name , setName] = useState("");
+  const [RollNo , setRollNo] = useState(0);
+
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
@@ -14,6 +21,7 @@ export default function form() {
             required
             className="w-full px-4 py-2 mt-1 text-sm border rounded-md focus:ring-blue-500 focus:border-blue-500 border-gray-300"
             placeholder="Enter your name"
+            onChange={(e)=>{setName(e.target.value)}}
           />
         </div>
         <div className="mb-6">
@@ -27,11 +35,13 @@ export default function form() {
             required
             className="w-full px-4 py-2 mt-1 text-sm border rounded-md focus:ring-blue-500 focus:border-blue-500 border-gray-300"
             placeholder="Enter your roll number"
+            onChange={(e)=>{setRollNo(Number(e.target.value))}}
           />
         </div>
         <button
           type="submit"
           className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+          
         >
           Submit
         </button>
